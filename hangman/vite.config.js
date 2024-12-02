56c14be80fe2de72ae2db1f2e80d10408edb6923
JoroScript <gorko7878@abv.bg>
2024-12-02 22:02:58 +0200

    done

--- hangman/vite.config.js
+++ hangman/vite.config.js
@@ -1,7 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
+
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['5left.png', '4left.png', '3left.png','2left.png','1left.png','0left.png'], // Add your image paths here
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,svg}'],
+      },
+    }),
+  ],
+
 })
